e16cf750e7a9d0faaf9c69d860f01888abfb3cf0
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-10 23:57:30 +0000

    Update repeat_adder.c
    prints time

--- PHY1090/week2/repeat_adder.c
+++ PHY1090/week2/repeat_adder.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char **argv) 
 {
+	clock_t on = clock();
 	// creates and initialises the variables
 	int i, input, output, multiple;
 	i = input = output = multiple = 0;
@@ -32,4 +33,6 @@ int main(int argc, char **argv)
 	// prints the output
 	printf("%d\n", output);
 	return 0;
+	clock_t off = clock()
+	printf("Elapsed: %f seconds\n", (double)(off - on) / CLOCKS_PER_SEC);
 }
