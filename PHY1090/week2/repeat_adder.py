7d6a369c371607740255ffd46ad9eddfd9d0b497
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-20 17:33:58 +0000

    Update repeat_adder.py

--- PHY1090/week2/repeat_adder.py
+++ PHY1090/week2/repeat_adder.py
@@ -7,23 +7,23 @@ def main():
 	output = 0
 	try:
 		# checks if there are the right number of arguments
-	# 	# converts the first two arguments to integers
- #                in_arg = int(sys.argv[1])
-	# 	multiple = int(sys.argv[2])
- #        except: # (argc != 2)
-	# 	# raises an error
-	# 	raise Exception("Incorrect arguments.\nUsage: python repeat_adder.py [NUM] [NUM]\ne.g.\npython repeat_adder.py 2 3")
+		# converts the first two arguments to integers
+                in_arg = int(sys.argv[1])
+		multiple = int(sys.argv[2])
+        except: # (argc != 2)
+		# raises an error
+		raise Exception("Incorrect arguments.\nUsage: python repeat_adder.py [NUM] [NUM]\ne.g.\npython repeat_adder.py 2 3")
 	
-	# # iterates over all numbers up to the input
- #        for i in range(in_arg):
-	# 	# adds the index to the output
-	# 	output = output + multiple
+	# iterates over all numbers up to the input
+        for i in range(in_arg):
+		# adds the index to the output
+		output = output + multiple
 	
-	# # prints the output
-	# print("{}\n".format(output))
-	# end_time = time.time()
-	# run_time = end_time - start_time
-	# print(f'run_time = {run_time}')
+	# prints the output
+	print("{}\n".format(output))
+	end_time = time.time()
+	run_time = end_time - start_time
+	print(f'run_time = {run_time}')
 	
 if __name__ == "__main__":
         main()
