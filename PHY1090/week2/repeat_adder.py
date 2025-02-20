356f6a022e1f37eadd8f990f9b27c89336be8166
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-20 16:08:50 +0000

    Update repeat_adder.py

--- PHY1090/week2/repeat_adder.py
+++ PHY1090/week2/repeat_adder.py
@@ -22,7 +22,7 @@ def main():
 	print("{}\n".format(output))
 	end_time = time.time()
 	run_time = end_time() - start_time()
-	print(f'run_time = {run_time}'
+	print(f'run_time = {run_time}')
 	
 if __name__ == "__main__":
         main()
