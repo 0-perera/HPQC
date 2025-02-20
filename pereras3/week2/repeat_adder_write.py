908083cf9327c1a8086f0343611e30da488be275
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-20 18:58:14 +0000

    Update repeat_adder_write.py

--- pereras3/week2/repeat_adder_write.py
+++ pereras3/week2/repeat_adder_write.py
@@ -25,7 +25,8 @@ def main():
 	run_time = end_time - start_time
 	return run_time
 
-output_file = open('output_times.txt', '+a')
+fn = 'C:/Users/Sofia/OneDrive - Dublin City University/24.25/quantum_computing/individual_tasks/week1'
+output_file = open(fn+'output_times.txt', '+a')
 if __name__ == "__main__":
         output_file.write(main())
 output_file.close()
