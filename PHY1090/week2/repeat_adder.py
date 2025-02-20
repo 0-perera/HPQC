49d024bec804786fd5f87840543cdccbc291dfa5
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-20 17:18:46 +0000

    Update repeat_adder.py

--- PHY1090/week2/repeat_adder.py
+++ PHY1090/week2/repeat_adder.py
@@ -8,17 +8,17 @@ def main():
 	print('fine2')
 	# checks if there are the right number of arguments
         try:
-		# converts the first two arguments to integers
-		in_arg = int(sys.argv[1])
-                multiple = int(sys.argv[2])
+	    # converts the first two arguments to integers
+	    in_arg = int(sys.argv[1])
+	    multiple = int(sys.argv[2])
         except: # (argc != 2)
-		# raises an error
-		raise Exception("Incorrect arguments.\nUsage: python repeat_adder.py [NUM] [NUM]\ne.g.\npython repeat_adder.py 2 3")
-		
+	    # raises an error
+	    raise Exception("Incorrect arguments.\nUsage: python repeat_adder.py [NUM] [NUM]\ne.g.\npython repeat_adder.py 2 3")
+	
 	# iterates over all numbers up to the input
         for i in range(in_arg):
-		# adds the index to the output
-		output = output + multiple
+	    # adds the index to the output
+	    output = output + multiple
 	
 	# prints the output
 	print("{}\n".format(output))
