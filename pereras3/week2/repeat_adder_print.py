25e356a42741b6f1dce461d985b72f4229b614aa
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-24 12:44:16 +0000

    Update repeat_adder_print.py

--- pereras3/week2/repeat_adder_print.py
+++ pereras3/week2/repeat_adder_print.py
@@ -1,20 +1,22 @@
 import sys
 import time
+import csv
 
 def main(): 
-	for i in range(0,4):
+	for goals in range(0,2):
 		#prints in terminal
-		if i==0:
+		if goals==0:
+			goal = 'read file input'
 			start_time = time.time()
-			input_file = open('input_file.csv', delimeter=',')
+			input_file = open('input_file.csv', 'a')
+			input_file.write('4, 5')
+			csv.reader(input_file, delimiter=' ')
 			in_arg = input_file[0]
 			multiple = input_file[1]
-			for i in range(in_arg):
-				# adds the index to the output
-				output = output + multiple
-				print("{}\n".format(output))
 		else:
+			goal = 'human input'
 			try:
+				start_time = time.time()
 				# checks if there are the right number of arguments
 				# converts the first two arguments to integers
 				in_arg = int(sys.argv[1])
@@ -22,28 +24,25 @@ def main():
 			except: # (argc != 2)
 				# raises an error
 				raise Exception("Incorrect arguments.\nUsage: python repeat_adder.py [NUM] [NUM]\ne.g.\npython repeat_adder.py 2 3")
-			for i in range(in_arg):
-				# adds the index to the output
-				output = output + multiple
-				print("{}\n".format(output))	
-				# iterates over all numbers up to the input
-
-			
-			
-		elif i==2:
+		for i in range(in_arg):
+			# adds the index to the output
+			output = output + multiple
+			print("{}\n".format(output))	
+			# iterates over all numbers up to the input
 		
 		# prints the output
-		
 		end_time = time.time()
 		run_time = end_time - start_time
-		if i==0:
-			print(f'run_time in sec = {run_time}')
-		if i==1:
-			cwd = Path.cwd()
-			fn = '/home/ug/pereras3/HPQC/pereras3/week2'
-			print(fn)
-			output_file = open(fn+'/output_times.txt', 'a')
-			
+		print(f'run_time in sec = {run_time}')
+		
+		output_text = open('/output_times.txt', 'a')
+		out_file = open('/output_times.csv', 'a')
+		output_text.writelines(f'for {goal} of {in_arg} x {multiple}, the runtime was {run_time}s.\n}')
+		output_file.writelines(f'{goal}, {in_arg}, {multiple} ./n')
+		output_file.close()
+		output_tetx.close()
+		
+		
 
 if __name__ == "__main__":
         main()
