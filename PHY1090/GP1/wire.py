42fb32f8e2dd82736cf8bd878e0f0bbae82db995
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-28 10:01:31 +0000

    Update wire.py

--- PHY1090/GP1/wire.py
+++ PHY1090/GP1/wire.py
@@ -202,7 +202,7 @@ def run(self):
 #checks the queue length and paths, plots the electrons in queue vs the number of paths
             if path_counter%update_interval == 0:
 		 queue_length.append(len(queue))
-		    paths.append(len(queue_length)*update_interval )
+		 paths.append(len(queue_length)*update_interval )
 		    plt.figure( 1 )
 		    plt.cla()
 		    plt.tick_params(axis='both', which='major', labelsize=plt_labsiz)
