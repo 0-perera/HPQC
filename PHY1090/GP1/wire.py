aaab9a20cc7bd52368a566db7ed2c58d13492df6
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-28 10:09:25 +0000

    Update wire.py

--- PHY1090/GP1/wire.py
+++ PHY1090/GP1/wire.py
@@ -200,13 +200,13 @@ def run(self):
             while not self.scatter(r, queue, self.electrons):
                 shape.sample(r)
 #checks the queue length and paths, plots the electrons in queue vs the number of paths
-            if path_counter%update_interval == 0:
-		queue_length.append(len(queue))
-		paths.append(len(queue_length)*update_interval )
-		plt.figure( 1 )
-		plt.cla()
-		plt.tick_params(axis='both', which='major', labelsize=plt_labsiz)
-		plt.plot( paths , np.array( queue_length ) )
+  #           if path_counter%update_interval == 0:
+		# queue_length.append(len(queue))
+		# paths.append(len(queue_length)*update_interval )
+		# plt.figure( 1 )
+		# plt.cla()
+		# plt.tick_params(axis='both', which='major', labelsize=plt_labsiz)
+		# plt.plot( paths , np.array( queue_length ) )
 	    if len(paths) > 2:
 			p , s = exponential().fit( paths, np.array( queue_length ) )
 			grow.append( p[1] )
