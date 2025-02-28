bfbcf842d47d6c8cfb76d1bc868d21b3ac2d8d21
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-02-28 09:29:52 +0000

    Update wire.py

--- PHY1090/GP1/wire.py
+++ PHY1090/GP1/wire.py
@@ -142,36 +142,36 @@ class ElectronElectron:
 
     def scatter(self, position, queue, electrons):
 	# Constants:
-        k_e=8.99e9        # Coulomb's constant (nm^2/C^2 )
-        e_charge=1.6e-19  # Electrons charge
-
-        # move the elctron in the random direction given by the def unit_vector
-        move_x, move_y, move_z = unit_vector()
-        new_position = (position[0] + move_x * random.uniform(0, 1),
-                    position[1] + move_y * random.uniform(0, 1))
+	k_e=8.99e9        # Coulomb's constant (nm^2/C^2 )
+	e_charge=1.6e-19  # Electrons charge
+	
+	# move the elctron in the random direction given by the def unit_vector
+	move_x, move_y, move_z = unit_vector()
+	new_position = (position[0] + move_x * random.uniform(0, 1),
+		    position[1] + move_y * random.uniform(0, 1))
 	    
 	# Evaluate Coulomb's repulsion of the elcectrons and the ones sourranding it
 	for electron in electrons:
 		if electron != position: # avoid electrons which are in the same location, or self-interaction
 			distance = np.linalg.norm(np.array(new_position) - np.array(electron))  # distance between electrons
-              	   	# if electrons are too close,  calculate repulsion, else: no interaction
-	       		if distance < 1e-10: 
-		  	force = k_e * (e_charge ** 2) / distance ** 2
-		  	# modify the direction because of coulombs law
-		  	move_x += force * (new_position[0] - electron[0])
-		  	move_y += force * (new_position[1] - electron[1])
+			# if electrons are too close,  calculate repulsion, else: no interaction
+			if distance < 1e-10: 
+			force = k_e * (e_charge ** 2) / distance ** 2
+			# modify the direction because of coulombs law
+			move_x += force * (new_position[0] - electron[0])
+			move_y += force * (new_position[1] - electron[1])
 		       
-		  	#update the position of the electron after Coulomb interaction
-	    	  	#new_position[0] += force_x 
-		  	#new_position[1] += forfe_y
+			#update the position of the electron after Coulomb interaction
+			#new_position[0] += force_x 
+			#new_position[1] += forfe_y
 		       
 	ended = False
 	# make sure electrons are still inside of the wire
-        if not self.wire.inside(new_position):
-            ended = True # dont save the new position
-        else:
-            queue.appendleft(new_position)  # Store the new position
-        return ended	
+	if not self.wire.inside(new_position):
+	    ended = True # dont save the new position
+	else:
+	    queue.appendleft(new_position)  # Store the new position
+	return ended	
 
 
 
