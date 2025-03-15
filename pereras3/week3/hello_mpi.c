b80cdb7d1b2b712342679d21ccb79e02e0d4cf4e
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-03-15 16:07:46 +0000

    Update hello_mpi.c

--- pereras3/week3/hello_mpi.c
+++ pereras3/week3/hello_mpi.c
@@ -12,12 +12,12 @@ int main(int argc, char **argv)
 
 	// intitalise MPI
 	ierror = MPI_Init(&argc, &argv);
-
+	
 	// gets the rank and world size
 	ierror = MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
 	ierror = MPI_Comm_size(MPI_COMM_WORLD,&uni_size);
 
-	for (int i = 1; i <= uni_size; i++) {
+	for (int i = 0; i <= uni_size; i++) {
 		MPI_Barrier(MPI_COMM_WORLD);
 		if (i == my_rank) {
 			printf("Hello from process %d out of %d\n", my_rank, uni_size);
