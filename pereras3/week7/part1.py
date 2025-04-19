4b7b34d5e2ec7e7ca2a365bf97936566b1c4085d
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-04-19 13:16:15 +0100

    Update part1.py

--- pereras3/week7/part1.py
+++ pereras3/week7/part1.py
@@ -72,9 +72,7 @@ def probQubit(name):
     tosQubit(name)
     workspace = np.reshape(workspace,(-1,2))
     prob = np.linalg.norm(workspace,axis=0)**2
-    print('prob sum is {}'.format(prob.sum())
-    workspace = np.reshape(workspace,(-1,2))
-    prob = np.linalg.norm(workspace,axis=0)**2
+    print('prob sum is {}'.format(prob.sum()))
     return prob                 # make sure sum is one
 
 def measureQubit(name): 
