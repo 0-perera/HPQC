e5d6a6e7e03cf9b48c579eed8e1c48264a599bad
pereras3 <153281051+0-perera@users.noreply.github.com>
2025-04-21 03:13:28 +0100

    Update hello_world.py

--- pereras3/week2/hello_world.py
+++ pereras3/week2/hello_world.py
@@ -1,13 +1,1 @@
-#include <stdio.h>
-
-// Based on Parewa Labs Pvt. Ltd. 
-// Available online at
-// https://www.programiz.com/c-programming/examples/print-sentence
-// 2025-01-09
-
-int main() 
-{
-   // printf() displays the string inside quotation
-   printf("Hello, World!\n");
-   return 0;
-}
+print("Hello, World!")
